cc06a0f5a7e1d88edf2540bf969b05650c0d1eca
ShubhamMeena07 <110387821+ShubhamMeena07@users.noreply.github.com>
2025-07-05 22:01:44 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sendContactEmail } = require('../controllers/contactController');
-
-const router = express.Router();
-
-router.post('/', sendContactEmail);
-
-module.exports = router;
